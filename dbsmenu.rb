48d7161dde031c0c481f7d1c831dcaab0330a7a0
kepler <githubkepler.50s@gishpuppy.com>
2019-02-22 14:52:14 +0530

    add class for sqlite3 and print rows

--- dbsmenu.rb
+++ dbsmenu.rb
@@ -5,10 +5,11 @@
 #                for current line.
 #       Author:  r kumar
 #         Date: 2019-02-21 - 12:27
-#  Last update: 2019-02-21 23:43
+#  Last update: 2019-02-22 14:49
 #      License: MIT License
 # ----------------------------------------------------------------------------- #
 #
+require 'logger'
 require './smenu.rb'
 #require 'sqlite3'
 # get_first_value  get_first_row
@@ -19,27 +20,42 @@ require 'color' # see ~/work/projects/common/color.rb
 #today = Date.today.to_s
 #now = Time.now.to_s
 # include? exist? each_pair split gsub each_with_index
+logger = Logger.new('logfile.log')
+_MAX_ROWS = 5
+# class that encapsulates Sqlite3 functions using command line client not gem
+class Cli_Sqlite3 # {{{
+  def initialize dbname
+    @dbname = nil
+    if File.exist? dbname
+      @dbname = dbname
+    else
+      raise ArgumentError, "#{dbname} does not exist."
+    end
+  end
 
-# @return Array
-def tables dbname
-  tables=%x{ sqlite3 #{dbname} ".tables" | tr -s ' ' | tr ' ' '\n' }
-  return tables.split("\n")
-end
-# @return Array
-def columns dbname, tbname
-  cols = %x{sqlite3 #{dbname} "PRAGMA table_info(#{tbname})" | cut -f2 -d'|'}
-  return cols.split("\n")
-end
-def row_count dbname, tbname
-  res = %x{sqlite3 #{dbname} "SELECT COUNT(1) from #{tbname}" }
-  res.chomp
-end
+  # @return Array of tablenames
+  def tables 
+    tables=%x{ sqlite3 #{@dbname} ".tables" | tr -s ' ' | tr ' ' '\n' }
+    return tables.split("\n")
+  end
+  # @return Array of columnnames
+  def columns tbname
+    cols = %x{sqlite3 #{@dbname} "PRAGMA table_info(#{tbname})" | cut -f2 -d'|'}
+    return cols.split("\n")
+  end
 
-def errecho text
-  $stderr.puts text
-end
+  # @return Integer count of rows
+  def row_count tbname
+    res = %x{sqlite3 #{@dbname} "SELECT COUNT(1) from #{tbname}" }
+    res.chomp.to_i
+  end
+  def execute statement
+    res = %x{sqlite3 #{@dbname} "#{statement}" }
+    return res.split("\n")
+  end
+end # class  }}}
 
-# break up array into columns. currently they are just appended not sized.
+# break up array into columns. currently they are just appended not sized. {{{
 # Another variation can be to create arrays rather than a string, so caller can decide
 # n is the number of rows to create, which have concatenated values
 def transpose arr, n
@@ -49,8 +65,29 @@ def transpose arr, n
     narr[ix%n] << " #{e} "
   end
   narr
-end
+end # }}}
 
+    def printcols cols
+      ncols = transpose(cols, 3)
+      3.times {|j| 
+        system "tput el"
+        puts ncols[j] 
+      }
+    end
+    def printdata cols, selected, db, max_rows=3
+      columns = cols[0,3].join(",")
+      statement = "SELECT #{columns} FROM #{selected} LIMIT #{max_rows}"
+      #logger.info(statement)
+      res = db.execute(statement)
+      #logger.debug res.class
+      res = res.join("\n")
+      rs = %x{ echo "#{res}" | column -t -s'|' -c 80 }
+      rs = rs.split("\n")
+      rs.each do |e|
+        system "tput el"
+        puts e
+      end
+    end
 
 if __FILE__ == $0
   include Color
@@ -60,7 +97,7 @@ if __FILE__ == $0
   $opt_quiet = false
   begin
     # http://www.ruby-doc.org/stdlib/libdoc/optparse/rdoc/classes/OptionParser.html
-    require 'optparse'
+    require 'optparse' # {{{
     options = {}
     OptionParser.new do |opts|
       opts.banner = "Usage: #{$0} [options]"
@@ -76,7 +113,7 @@ if __FILE__ == $0
       opts.on("-q", "--quiet", "Run quietly") do |v|
         $opt_quiet = true
       end
-    end.parse!
+    end.parse! # }}}
 
     p options if $opt_debug
     p ARGV if $opt_debug
@@ -87,31 +124,34 @@ if __FILE__ == $0
       $stderr.puts "File: #{filename} does not exist. Aborting"
       exit 1
     end
-    _tables = tables(filename)
+    db = Cli_Sqlite3.new filename
+    _tables = db.tables()
     #puts "#{_tables} #{_tables.count}"
     menu = Smenu.new
+    scrollrows = _MAX_ROWS + 1
     selected = menu.run _tables do |ix|
       tb = _tables[ix]
-      cols = columns(filename, tb)
-      rows = row_count(filename, tb)
+      cols = db.columns(tb)
+      rows = db.row_count(tb)
       print color( "#------ columns #{cols.size} rows: #{rows} -------\n", "bold")
-      ncols = transpose(cols, 3)
-      3.times {|j| 
-        system "tput el"
-        puts ncols[j] 
-      }
-      4.times {|j| system "tput cuu1;" }
+      system "tput ed"
+      #printcols cols
+      printdata cols, tb, db, _MAX_ROWS
+      scrollrows.times {|j| system "tput cuu1;" }
       system "tput ed"
     end
-      4.times {|j| system "tput cud1;" }
+      scrollrows.times {|j| system "tput cud1;" }
     if selected
-      cols = columns(filename, selected)
+      cols = db.columns(selected)
       system "tput ed"
+      printdata cols, selected, db, _MAX_ROWS
+      if false
       puts
       puts "SELECT"
       puts cols.join(',')
       puts "FROM     "
       puts selected
+      end
     end
 
 
