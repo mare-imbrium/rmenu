7087a5cb237f73f8ff7315ef393ea363e186c209
kepler <githubkepler.50s@gishpuppy.com>
2019-02-26 23:28:27 +0530

    smenu.rb: clean up

--- smenu.rb
+++ smenu.rb
@@ -8,7 +8,7 @@
 #       Author: j kepler  http://github.com/mare-imbrium/
 #         Date: 2019-02-21 - 09:33
 #      License: MIT
-#  Last update: 2019-02-25 15:04
+#  Last update: 2019-02-26 23:16
 # ----------------------------------------------------------------------------- #
 #  smenu.rb  Copyright (C) 2012-2019 j kepler
 # v1 - printed all lines after each press resulting in flicker
@@ -29,10 +29,19 @@ require 'io/wait'
 require 'io/console'
 
 class Smenu
+
+  # how many lines to display
   attr_accessor :display_lines
+
+  # message to print before choices
   attr_accessor :message
+
+  # TODO transpose choices
   attr_accessor :transpose_flag
+
+  # block to call when ENTER pressed
   attr_accessor :on_enter_block
+
   def initialize
     @display_lines = 5
     @message = nil
@@ -44,11 +53,13 @@ class Smenu
     ## get input from keyboard, as there could be STDIN from pipe.
     @ios = tty_open
   end
+
+  # store block to execute if ENTER pressed
   def on_enter &block
     @on_enter_block = block
   end
 
-  # we require this in case user pipes in data
+  # Use tty in place of stdin, in case user pipes in data
   def tty_open
     fd = IO.sysopen "/dev/tty", "r"
     ios = IO.new(fd, "r")
@@ -62,13 +73,15 @@ class Smenu
     @ios.getch
   end
 
-  def char_if_pressed  # {{{
+  # get a character/key from user and process
+  # @return String c.chr. e,g, ENTER TAB C-n a b c 1 2 3 
+  def getchar  # {{{
     #cn = nil
     begin
       system("stty raw -echo 2>/dev/null") # turn raw input on
       #c = $stdin.getc
       c = tty_getc
-      if c == ''
+      if c == ''      # Escape character, possible arrow key or page down
         buff = c.chr
         while true
           k = nil
@@ -83,17 +96,19 @@ class Smenu
           end
         end
       end
+
+      # check for control characters
       cn = c.ord
       if cn >= 0 and cn < 27
-      case cn
-      when 13,10
-        return "ENTER"
-      when 9
-        return "TAB"
-      else
-        x = cn + 96
-        return "C-#{x.chr}"
-      end
+        case cn
+        when 13,10
+          return "ENTER"
+        when 9
+          return "TAB"
+        else
+          x = cn + 96
+          return "C-#{x.chr}"
+        end
       end
       c.chr if c
     ensure
@@ -101,13 +116,17 @@ class Smenu
     end
   end  # }}}
 
-  def fix_rows index, ch
+  # fix start and finish index when scrolling
+  # modifies start_row and last_row
+  # @param Integer current index
+  # @param Array choices
+  def fix_rows index, choices
     @last_row = @start_row + @display_lines - 1
     if index > @last_row 
       @start_row = index
       @last_row += (@display_lines - 0)
-      if @last_row > ch.size - 1
-        @last_row = ch.size - 1
+      if @last_row > choices.size - 1
+        @last_row = choices.size - 1
         @start_row = (@last_row - @display_lines) + 1
       end
     elsif index < @start_row
@@ -117,12 +136,12 @@ class Smenu
     end
   end
 
-  def print_full ch, index   # {{{
-    if ch.size < @display_lines
-      @display_lines = ch.size
+  def print_full choices, index   # {{{
+    if choices.size < @display_lines
+      @display_lines = choices.size
     end
-    fix_rows index, ch
-    ch.each_with_index { |e, ix|
+    fix_rows index, choices
+    choices.each_with_index { |e, ix|
       next if ix < @start_row
       next if ix > @last_row
       if ix == index
@@ -136,17 +155,17 @@ class Smenu
   end  # }}}
 
   # now unused since scrolling requires reprinting all
-  def UNUSEDprint_partial ch, prev_index, index  # {{{
+  def UNUSEDprint_partial choices, prev_index, index  # {{{
     raise
     # 
     # only print for two given indices
     # there is still some flicker since I am still printing a puts for the non lines
     # I don't know current cursor pos, so i can just jump to that position
-    fix_rows index, ch
-    lines = ch.size 
+    fix_rows index, choices
+    lines = choices.size 
     lines = @display_lines
     lines.times { system "tput cuu1;" }
-    ch.each_with_index { |e, ix|
+    choices.each_with_index { |e, ix|
       next if ix < @start_row
       next if ix > @last_row
       marker = nil
@@ -165,7 +184,7 @@ class Smenu
     }
   end   # }}}
 
-  def run ch
+  def run choices
     Signal.trap("INT") do # SIGINT = control-C
       reset_terminal
       exit
@@ -182,13 +201,13 @@ class Smenu
   
         if printed
           # after first time
-          #print_partial ch, prev_index, index
+          #print_partial choices, prev_index, index
           lines = @display_lines
           lines.times { system "tput cuu1;" }
-          print_full(ch, index) 
+          print_full(choices, index) 
         else
           # first time
-          print_full(ch, index) unless printed
+          print_full(choices, index) unless printed
           printed = true 
         end
         # TODO save cursor here
@@ -196,7 +215,7 @@ class Smenu
         yield index , c if block_given?
         # TODO restore cursor here
         #system "tput rc"
-        c = char_if_pressed
+        c = getchar
         next unless c
 
 
@@ -211,25 +230,25 @@ class Smenu
           if @on_enter_block
             @on_enter_block.call(index)
           else
-            return ch[index]
+            return choices[index]
           end
-        when "[B"
+        when "[B"     # down arrow
           index += 1
-        when "[A"
+        when "[A"     # up arrow
           index -= 1
-        when "[C"
+        when "[C"     # right arraw
           index += 1
-        when "[D"
+        when "[D"     # left arrow
           index -= 1
         when "[H"
           index = 0           # avoid this since we only want two consecutive lines affected
         when "[F"
-          index = ch.size - 1 # avoid this since we only want two consecutive lines affected
+          index = choices.size - 1 # avoid this since we only want two consecutive lines affected
         else
           ; # nothing yet
         end
         index = 0 if index < 0
-        index = ch.size-1 if index > ch.size-1
+        index = choices.size-1 if index > choices.size-1
       end
     ensure
       reset_terminal
